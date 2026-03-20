3e6db067db51cd6690fa69990a2468c1ec6d6409
BlazeKing13 <leon.mermolja@gmail.com>
2026-03-20 12:19:26 +0100

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -86,7 +86,7 @@ function longToHTML(text) {
 
       const title = escapeHTML(post.title);
       const date = formatDate(post.date);
-      const short = ev.short ? marked.parse(ev.short) : "";
+      const short = post.short ? marked.parse(post.short) : "";
       const img = escapeHTML(
         normalizeAsset(post.image || post.img || "./images/TCS.jpg"),
       );
